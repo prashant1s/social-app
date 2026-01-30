6fb9d87b2afbfe749b37b214a6bebb1af0f67a61
prashant1s <prashantextra2003@gmail.com>
2026-01-30 16:25:46 +0530

    port commit

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,11 @@ app.use("/api/posts", require("./routes/post.js"));
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
+    const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+
+   
   })
   .catch(err => console.error(err));
