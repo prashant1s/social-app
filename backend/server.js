093a3194a0ae029de9a692ffc26d3e55bac7a1f2
prashant1s <prashantextra2003@gmail.com>
2026-01-30 16:37:45 +0530

    login route

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", require("./routes/auth.js"));
+app.use("/api/login", require("./routes/auth.js"));
 app.use("/api/posts", require("./routes/post.js"));
 
 mongoose
