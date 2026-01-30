cc5470275fa47796baca43d9023fb106d3026413
prashant1s <prashantextra2003@gmail.com>
2026-01-30 16:52:17 +0530

    login rout1e

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,16 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors("https://social-app-five-gules.vercel.app/login"));
 app.use(express.json());
+app.use(express.urlrncoded({extended:true}));
 
+app.use('/',(req,res)=>{
+  res.send({
+    activeStatus:true,
+    error:false,
+  })
+})
 app.use("/api/login", require("./routes/auth.js"));
 app.use("/api/posts", require("./routes/post.js"));
 
