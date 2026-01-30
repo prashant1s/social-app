1a5e851ee40d756523d4e6000e5acac204cfa2ca
prashant1s <prashantextra2003@gmail.com>
2026-01-30 16:29:52 +0530

    port commit1

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,19 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const app  = express();
-
+const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", require("./routes/auth.js"));
 app.use("/api/posts", require("./routes/post.js"));
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-
-   
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
