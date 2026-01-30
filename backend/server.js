e230eff6ea990ba2d7a714fee341ba8d7476ee7b
prashant1s <prashantextra2003@gmail.com>
2026-01-30 19:22:30 +0530

    login route bug1

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const path = require(path);
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 // app.use(cors("https://social-app-five-gules.vercel.app"));
