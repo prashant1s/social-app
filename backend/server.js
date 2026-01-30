f08771ebb8c07b3eaa86a01cc766fabfdcecfd81
prashant1s <prashantextra2003@gmail.com>
2026-01-30 19:26:49 +0530

    login route bug22

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 // app.use(cors("https://social-app-five-gules.vercel.app"));
-dotenv.config();
+
 app.use(cors({
   origin: "https://social-app-five-gules.vercel.app",
   credentials: true,
